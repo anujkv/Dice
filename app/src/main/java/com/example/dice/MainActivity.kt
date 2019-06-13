209aa58ab2aa6f5e64f8943b4921c02ae35c487c
anujkv <anujkrvaish@gmail.com>
2019-06-13 11:43:55 +0530

    make variables globally

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -6,16 +6,17 @@ import android.view.View
 import android.widget.*
 import java.util.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
+    private lateinit var rollbtn : Button
+    private lateinit var rollResult : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var rollbtn : Button = findViewById(R.id.rollbtn)
+        rollbtn  = findViewById(R.id.rollbtn)
+        rollResult  = findViewById(R.id.diceView)
 
         rollbtn.setOnClickListener(){
-             val randomValue = (Random().nextInt(6)+1)
-             val drawableResource = when(randomValue){
+             val drawableResource = when((Random().nextInt(6)+1)){
                  1 -> R.drawable.dice_1
                  2 -> R.drawable.dice_2
                  3 -> R.drawable.dice_3
@@ -23,18 +24,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                  5 -> R.drawable.dice_5
                  else -> R.drawable.dice_6
              }
-             val rollResult : ImageView = findViewById(R.id.diceView)
+
              rollResult.setImageResource(drawableResource)
          }
 
     }
 
     override fun onClick(p0: View?) {
-
         when(p0?.id){
             R.id.rollbtn -> {
-                val randomValue = (Random().nextInt(6)+1)
-                val drawableResource = when(randomValue){
+                val drawableResource = when((Random().nextInt(6)+1)){
                     1 -> R.drawable.dice_1
                     2 -> R.drawable.dice_2
                     3 -> R.drawable.dice_3
@@ -42,10 +41,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     5 -> R.drawable.dice_5
                     else -> R.drawable.dice_6
                 }
-                val rollResult : ImageView = findViewById(R.id.diceView)
                 rollResult.setImageResource(drawableResource)
 
             }
         }
     }
 }
+
+
